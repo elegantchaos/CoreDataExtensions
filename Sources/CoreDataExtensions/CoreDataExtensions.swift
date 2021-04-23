73ee2d9da62d4656682efe6307ce0c68167d10fe
Sam Deane <sam@elegantchaos.com>
2021-04-23 17:08:18 +0100

    Removed stub

--- Sources/CoreDataExtensions/CoreDataExtensions.swift
+++
@@ -1,8 +0,0 @@
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-//  Created by Sam Deane on 22/04/2021.
-//  All code (c) 2021 - present day, Elegant Chaos.
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-struct CoreDataExtensions {
-    var text = "Hello, World!"
-}
